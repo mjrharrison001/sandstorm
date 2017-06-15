c357ef5f0ebe3e69a13c4c1d441d7ba3c65febb0
Michael <mjrharrison001@gmail.com>
2017-06-14 21:49:42 -0400

    prod mode test

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express         = require('express');
+var path            = require('path');
+var favicon         = require('serve-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var mongoose        = require('mongoose');
 
-var appRoutes = require('./routes/app');
+var appRoutes       = require('./routes/app');
 
 var app = express();
+
+/**
+* connect to MongoDB
+**/
+const uristring = process.env.MONGODB_URI;
+mongoose.connect(uristring, function (err, db) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
